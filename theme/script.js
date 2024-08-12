15c24b80f0632409101cba93d6491e2c9346caa1
Jyotika Kishor <jkishor1802@gmail.com>
2024-08-12 23:36:27 +0530

    Updated theme toggle HTML, CSS, and JavaScript files with minor formatting changes/

--- theme/script.js
+++ theme/script.js
@@ -1,25 +1,21 @@
+const themeSwitch = document.getElementById('theme-switch');
+const body = document.body;
 
-    // Get the theme switch input and the body element
-    const themeSwitch = document.getElementById('theme-switch');
-    const body = document.body;
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-theme');
+    themeSwitch.checked = true;
+} else {
+    body.classList.add('light-theme');
+}
 
-    // Check for saved theme in local storage and apply it
-    if (localStorage.getItem('theme') === 'dark') {
+themeSwitch.addEventListener('change', function () {
+    if (themeSwitch.checked) {
+        body.classList.remove('light-theme');
         body.classList.add('dark-theme');
-        themeSwitch.checked = true;
+        localStorage.setItem('theme', 'dark');
     } else {
+        body.classList.remove('dark-theme');
         body.classList.add('light-theme');
+        localStorage.setItem('theme', 'light');
     }
-
-    // Add an event listener to the theme switch
-    themeSwitch.addEventListener('change', function() {
-        if (themeSwitch.checked) {
-            body.classList.remove('light-theme');
-            body.classList.add('dark-theme');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            body.classList.remove('dark-theme');
-            body.classList.add('light-theme');
-            localStorage.setItem('theme', 'light');
-        }
-    });
+});
