95d53d1c5481fbc6770a97697438c29909c8a2ab
Jyotika Kishor <jkishor1802@gmail.com>
2024-08-17 00:30:22 +0530

    feat: Add arrow function examples

---
+++ arrowfunction.js
@@ -0,0 +1,16 @@
+//Normal Function
+
+function add(a, b) {
+    return a + b;
+}
+
+//Arrow function
+const add1 = (a, b) => {
+    return a + b;
+}
+
+//Implicit return using arrow function
+const add2 = (a, b) => a + b;
+
+//Arrow function with no arguments
+const gre̥et = () => "hello world";
